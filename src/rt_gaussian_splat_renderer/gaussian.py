7683ada04d23bd2f07a10425ea8f64c042e1a0db
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-03-01 18:14:13 -0800

    Added Gaussian Taichi scope init

--- src/rt_gaussian_splat_renderer/gaussian.py
+++ src/rt_gaussian_splat_renderer/gaussian.py
@@ -13,10 +13,28 @@ class Gaussian:
     rotation: ti.math.vec4
     scale: ti.math.vec3
 
+    @ti.func
+    def init(self,
+             position=ti.math.vec3(0, 0, 0),
+             rotation=ti.math.vec4(1, 0, 0, 0),
+             scale=ti.math.vec3(1, 1, 1)):
+        """Taichi scope Gaussian initialization method.
+
+        :param position: gaussian center (mean).
+        :type position: ti.math.vec3
+        :param rotation: gaussian rotation quaternion.
+        :type rotation: ti.math.vec4
+        :param scale: gaussian scale.
+        :type scale: ti.math.vec3
+        """
+        self.position = position
+        self.rotation = rotation
+        self.scale = scale
+
 
 def new_gaussian(position: ti.math.vec3 = ti.math.vec3(0, 0, 0),
-                 rotation: ti.math.vec4 = ti.math.vec4(0, 0, 0, 1),
-                 scale: ti.math.vec3 = ti.math.vec3(1, 1, 1)) -> Gaussian:  # pyright: ignore
+                 rotation: ti.math.vec4 = ti.math.vec4(1, 0, 0, 0),
+                 scale: ti.math.vec3 = ti.math.vec3(1, 1, 1)) -> Gaussian:
     """Python scope Gaussian constructor to create a new Gaussian.
 
     :param position: gaussian center (mean).
