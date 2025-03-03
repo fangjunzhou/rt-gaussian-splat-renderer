475c73e20f6664b0e1f80d41c373ee1ec9934d42
Zixin Xu <zx35@cornell.edu>
2025-03-03 09:18:14 -0800

    Removing tests from main

--- src/rt_gaussian_splat_renderer/__main__.py
+++ src/rt_gaussian_splat_renderer/__main__.py
@@ -1,5 +1,3 @@
-from camera import Camera
-from ray import Ray
 import taichi as ti
 import math
 
@@ -8,10 +6,4 @@ def main():
     print("Hello World!")
 
 if __name__ == "__main__":
-    position = ti.math.vec3(0.0, 2.0, 3.0)
-    looking_at = ti.math.vec3(0.0, 0.0, 0.0)
-    up = ti.math.vec3(0.0, 1.0, 0.0)
-    vert_fov = 90.0
-    aspect_ratio = 16.0 / 9.0
-    camera = Camera(position, looking_at,up, vert_fov, aspect_ratio)
     main()
