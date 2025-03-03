af6b06df45b51e06ec0c2485462d70a7cb6adee5
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-03-03 01:38:31 -0800

    Added Scene.load_file to load ply file

---
+++ tests/test_scene.py
@@ -0,0 +1,25 @@
+import logging
+from pathlib import Path
+import pytest
+import taichi as ti
+
+from rt_gaussian_splat_renderer.scene import Scene
+
+
+logger = logging.getLogger(__name__)
+
+# Init taichi runtime.
+ti.init(arch=ti.gpu, random_seed=42)
+logger.info(f"Current Taichi backend: {ti.cfg.arch}")  # pyright: ignore
+
+
+def test_scene_load():
+    """Test load Gaussian splat scene."""
+    scene = Scene()
+    try:
+        path = Path("tests/data/test.ply")
+        scene.load_file(path)
+    except Exception as e:
+        pytest.fail("Load scene failed.")
+
+# TODO: Handle illegal GS file.
