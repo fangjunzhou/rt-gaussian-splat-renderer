dcc33e69f3135c6e5319fa8f9bdc66b678b8038a
Fangjun Zhou <fangjun.zhou02@gmail.com>
2025-03-08 18:53:35 -0800

    Fixed Gaussian hit cov matrix term

--- src/rtgs/gaussian.py
+++ src/rtgs/gaussian.py
@@ -94,22 +94,25 @@ class Gaussian:
         :rtype: ti.math.vec2
         """
         # TODO: Implement Ray-Gaussian intersection.
-        A = ray.direction.dot(self.cov() @ ray.direction)
-        B = 2 * ray.direction.dot(self.cov() @ (ray.origin - self.position))
+        cov_inv = ti.math.inverse(self.cov())
+        A = ray.direction.dot(cov_inv @ ray.direction)
+        B = 2 * ray.direction.dot(cov_inv @ (ray.origin - self.position))
         # determine the threshold for intersection
-        thres=1e-6
-        C = (ray.origin - self.position).dot(self.cov() @ (ray.origin - self.position)) - thres
-        delta=B**2-4*A*C
+        thres = 1e-5
+        C = (ray.origin - self.position).dot(cov_inv @
+                                             (ray.origin - self.position)) - thres
+        delta = B**2 - 4 * A * C
         result = ti.math.vec2(ti.math.inf, ti.math.inf)
 
         if delta > 0:
             t1 = (-B + ti.sqrt(delta)) / (2 * A)
             t2 = (-B - ti.sqrt(delta)) / (2 * A)
-            result = ti.math.vec2(t1, t2) 
+            result = ti.math.vec2(t1, t2)
         elif delta == 0:
             t = -B / (2 * A)
             result = ti.math.vec2(t, ti.math.inf)
         return result
+
 
 def new_gaussian(position: ti.math.vec3 = ti.math.vec3(0, 0, 0),
                  rotation: ti.math.vec4 = ti.math.vec4(0, 0, 0, 1),
